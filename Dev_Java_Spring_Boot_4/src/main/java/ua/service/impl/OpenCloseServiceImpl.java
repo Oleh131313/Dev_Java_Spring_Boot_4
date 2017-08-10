ef5d394b2fae00b5bd95e6ec3f2906d23bc0d861
Oleh131313 <olegzavidovsky@gmail.com>
2017-08-10 17:57:48 +0300

    First Commit

---
+++ Dev_Java_Spring_Boot_4/src/main/java/ua/service/impl/OpenCloseServiceImpl.java
@@ -0,0 +1,18 @@
+package ua.service.impl;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ua.entity.OpenClose;
+import ua.repository.OpenCloseRepository;
+import ua.service.OpenCloseService;
+@Service
+public class OpenCloseServiceImpl extends CrudServiceImpl<OpenClose, Integer> implements OpenCloseService{
+
+	@Autowired
+	public OpenCloseServiceImpl(OpenCloseRepository repository) {
+		super(repository);
+	}
+
+}
