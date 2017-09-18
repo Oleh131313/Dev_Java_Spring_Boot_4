1726e0f4ffcfb0addd70969222af6c9221ba6546
Oleh131313 <olegzavidovsky@gmail.com>
2017-09-18 03:03:34 +0300

    add rendom cuisine(lesson 23)

--- Dev_Java_Spring_Boot_4/src/main/java/ua/DevJavaSpringBoot4Application.java
+++ Dev_Java_Spring_Boot_4/src/main/java/ua/DevJavaSpringBoot4Application.java
@@ -1,15 +1,15 @@
 package ua;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
-import ua.repository.CafeRepository;
 
 @SpringBootApplication
 public class DevJavaSpringBoot4Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevJavaSpringBoot4Application.class, args);
+		
 	}
 }
--- Dev_Java_Spring_Boot_4/src/main/java/ua/entity/Cuisine.java
+++ Dev_Java_Spring_Boot_4/src/main/java/ua/entity/Cuisine.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
