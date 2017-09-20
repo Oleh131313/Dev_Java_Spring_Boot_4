885b09687efa23e4fb2467fe12626559bef63fb7
Oleh131313 <olegzavidovsky@gmail.com>
2017-09-21 01:21:09 +0300

    add simple filter

--- Dev_Java_Spring_Boot_4/src/main/java/ua/controller/AdminCuisineController.java
+++ Dev_Java_Spring_Boot_4/src/main/java/ua/controller/AdminCuisineController.java
@@ -4,6 +4,8 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
 import ua.entity.Cuisine;
+import ua.model.filter.SimpleFilter;
 import ua.service.CuisineService;
 
 @Controller
@@ -34,35 +37,59 @@ private final CuisineService service;
 	@ModelAttribute("cuisine")
 	public Cuisine getForm(){
 		return new Cuisine();
-		
+	}
+	
+	@ModelAttribute("filter")
+	public SimpleFilter getFilter() {
+		return new SimpleFilter();
 	}
 	
 	@GetMapping
-	public String show(Model model, @PageableDefault Pageable pageable) {
-		model.addAttribute("cuisines", service.findAll(pageable));
+	public String show(Model model, @PageableDefault Pageable pageable, @ModelAttribute("filter") SimpleFilter filter) {
+		model.addAttribute("cuisines", service.findAll(pageable, filter));
 		return "cuisine";
 	}
 	
 	@GetMapping("/delete/{id}")
-	public String delete(@PathVariable Integer id) {
+	public String delete(@PathVariable Integer id, @PageableDefault Pageable pageable, @ModelAttribute("filter") SimpleFilter filter) {
 		service.delete(id);
-		return "redirect:/admin/cuisine";
+		return "redirect:/admin/cuisine"+buildParams(pageable, filter);
 	}
 	@PostMapping
-	public String save(	@ModelAttribute("cuisine") @Valid Cuisine cuisine, BindingResult br, Model model, SessionStatus status, @PageableDefault Pageable pageable) {
-		if(br.hasErrors()) return show(model, pageable);
+	public String save(	@ModelAttribute("cuisine") @Valid Cuisine cuisine, BindingResult br, Model model, SessionStatus status, @PageableDefault Pageable pageable, @ModelAttribute("filter") SimpleFilter filter) {
+		if(br.hasErrors()) return show(model, pageable, filter);
 		service.save(cuisine);
-		return cancel(status);
+		return cancel(status, pageable, filter);
 	}
 	@GetMapping("/update/{id}")
-	public String update(@PathVariable Integer id, Model model, @PageableDefault Pageable pageable){
+	public String update(@PathVariable Integer id, Model model, @PageableDefault Pageable pageable, @ModelAttribute("filter") SimpleFilter filter){
 		model.addAttribute("cuisine", service.findOne(id));
-		return show(model, pageable);
+		return show(model, pageable, filter);
 	}
 	@GetMapping("/cancel")
-	public String cancel(SessionStatus status){
+	public String cancel(SessionStatus status, @PageableDefault Pageable pageable, @ModelAttribute("filter") SimpleFilter filter){
 		status.setComplete();
-		return "redirect:/admin/cuisine";
+		return "redirect:/admin/cuisine"+buildParams(pageable, filter);
+	}
+	
+	private String buildParams(Pageable pageable, SimpleFilter filter) {
+		StringBuilder buffer = new StringBuilder();
+		buffer.append("?page=");
+		buffer.append(String.valueOf(pageable.getPageNumber()+1));
+		buffer.append("&size=");
+		buffer.append(String.valueOf(pageable.getPageSize()));
+		if(pageable.getSort()!=null){
+			buffer.append("&sort=");
+			Sort sort = pageable.getSort();
+			sort.forEach((order)->{
+				buffer.append(order.getProperty());
+				if(order.getDirection()!=Direction.ASC)
+				buffer.append(",desc");
+			});
+		}
+		buffer.append("&search=");
+		buffer.append(filter.getSearch());
+		return buffer.toString();
 	}
 		
 }
--- Dev_Java_Spring_Boot_4/src/main/java/ua/repository/CuisineRepository.java
+++ Dev_Java_Spring_Boot_4/src/main/java/ua/repository/CuisineRepository.java
@@ -1,9 +1,10 @@
 package ua.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import ua.entity.Cuisine;
 
-public interface CuisineRepository extends JpaNameRepository<Cuisine>{
+public interface CuisineRepository extends JpaNameRepository<Cuisine>, JpaSpecificationExecutor<Cuisine>{
 
 }
